818b6a172a3028dfeed89eecab3d1a1aee74b7ae
Simamkele11 <simamkelesokoyi@gmail.com>
2024-04-15 16:37:17 +0000

    ES6 is fun

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = fals;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    let task = false;
+    let task2 = true;
+  }
+
+  return [task, task2];
+}
