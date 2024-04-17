d5275a4933c77570ba01c302c5aeb0a25176893d
Simamkele11 <simamkelesokoyi@gmail.com>
2024-04-17 07:18:44 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = fals;
-  const task2 = true;
-
   if (trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    var task = true;
+    var task2 = false;
   }
+  
+  let task = true;
+  let task2 = false;
 
-  return [task, task2];
+  return [task2, task];
 }
